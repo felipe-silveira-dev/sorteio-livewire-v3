b44efa6b5f1ef68499fcfa8e2a5680415efa87d7
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-08-13 18:32:09 -0300

    fixing livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
 import './bootstrap';
 import JSConfetti from 'js-confetti';
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
 
 const jsConfetti = new JSConfetti();
 window.confetti = () => jsConfetti.addConfetti();
